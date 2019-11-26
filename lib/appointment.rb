f6f9c67cfde42279aaa15f45b716548ec293861c
4myc <missch3n@gmail.com>
2019-11-25 16:06:10 -0800

    done

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,5 +1,5 @@
 class Appointment #join
-    attr_accessor :doctor, :patient, :date #order doesn't match the README!, "appointments" test order is "date, patient, doctor"
+    attr_accessor :doctor, :patient, :date #order in README doesn't match the spec.rb!, "appointments" test order is "date, patient, doctor"
     @@all = []
 
     def initialize(doctor, patient, date)
